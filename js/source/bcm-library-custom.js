0580f72b49677b0f90b028ed30ef7c4f1500a200
Jaesung Song <jsong@berklee.edu>
2016-04-19 10:11:39 -0400

    Initial Commit

---
+++ js/source/bcm-library-custom.js
@@ -0,0 +1,6 @@
+// to replace/attache Onelogin logout link
+/*
+jQuery(document).ready(function(){
+	jQuery("#block-menu-menu-menu-login-menu ul.menu li:nth-child(2) a").attr("href", "/user/logout?current=https://berklee.onelogin.com/logout");
+});
+*/
