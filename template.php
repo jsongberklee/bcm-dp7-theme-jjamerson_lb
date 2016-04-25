52b267365de4a3a5bd802b8a4eeb9340d227e0ad
Jaesung Song <jsong@berklee.edu>
2016-04-25 15:43:10 -0400

    20160424

--- template.php
+++ template.php
@@ -46,7 +46,9 @@ function jjamerson_lb_preprocess_page(&$variables) {
       $current_node = $variables['node'];
     }
   } else {
-    $current_node = $variables['node'];
+    // by jsong
+    // original $current_node = $variables['node'];
+    $current_node = null;
   }
 
   // Check to see if this is a stand-alone site section. First we fetch the site section ID, if
@@ -94,11 +96,11 @@ function jjamerson_lb_preprocess_page(&$variables) {
   }
 
   if (drupal_is_front_page() ) {
-    drupal_add_js('//maps.googleapis.com/maps/api/js?libraries=geometry', array('type'  => 'external'));
+    //drupal_add_js('//maps.googleapis.com/maps/api/js?libraries=geometry', array('type'  => 'external'));
 
     // VideoJS components:
-    drupal_add_js('//vjs.zencdn.net/4.12/video.js', array('type'  => 'external', 'scope' => 'footer'));
-    drupal_add_css('//vjs.zencdn.net/4.12/video-js.css', array('type'  => 'external'));
+    //drupal_add_js('//vjs.zencdn.net/4.12/video.js', array('type'  => 'external', 'scope' => 'footer'));
+    //drupal_add_css('//vjs.zencdn.net/4.12/video-js.css', array('type'  => 'external'));
 
     /* inline JS for aligning <div> height matches with other divs, compatible with all major browsers, by jsong */
     /* on click attached to the selected blocks to make the whole area is clickable, by jsong */
